4673ff15b382cddaa7955472fdeffb7e4a091ad9
jlacs <joselacsoniii@gmail.com>
2022-08-09 19:08:53 +0800

    migration

---
+++ database/migrations/2022_08_09_022755_create_users.php
@@ -0,0 +1,41 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateUsers extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        // Code to create table
+        Schema::dropIfExists('users');
+        Schema::create('users', function (Blueprint $table) {
+            $table->engine = 'InnoDB';
+            $table->charset = 'utf8';
+            $table->bigInteger('id')
+                ->startingValue(2)
+                ->nullable(false)
+                ->unsigned()
+                ->unique()
+                ->primary();
+            $table->string('name', 255);
+            $table->text('comments');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('users');
+    }
+}
